9cd907eb053810d9ce6c890f5423cd8bf6e9c083
suudi abdisalan <abdisalansucdi@gmail.com>
2024-10-01 18:32:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(record){
+    const winRecord = record.find(game => game.result === "W");
+    return winRecord ? winRecord.year : undefined;
+}
+const record = [
+    {year: '2015', team: 'Denver Broncos', result: 'W'}, 
+    {year: '2014', team: 'Seattle Seahawks', result: 'L'}
+    
+    
+];
+console.log(superbowlWin(record));
+
+
+
